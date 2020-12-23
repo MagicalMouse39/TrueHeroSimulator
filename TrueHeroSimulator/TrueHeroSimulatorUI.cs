c6ed809b2d66c00b8b07d242948bee316d01d305
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2020-12-23 21:53:13 +0100

    Created and set up GUI

---
+++ TrueHeroSimulator/TrueHeroSimulatorUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrueHeroSimulator
+{
+    class TrueHeroSimulatorUI : Form
+    {
+        public TrueHeroSimulatorUI()
+        {
+            this.Width = 656;
+            this.Height = 515;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
